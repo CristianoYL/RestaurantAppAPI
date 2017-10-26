b71249d32d383cab31fa6635898552b1a36d4347
Li Yin <li.yin.cristiano@rutgers.edu>
2017-10-26 18:56:22 -0400

    sync code

--- code/app.py
+++ code/app.py
@@ -50,4 +50,4 @@ api.add_resource(MenuByID,'/menu/id/<int:id>')
 if __name__ == '__main__' :
     from db import db
     db.init_app(app)
-    app.run(host = '192.168.1.8',port = 5000,debug=True)
+    app.run(host = '192.168.0.102',port = 5000,debug=True)
