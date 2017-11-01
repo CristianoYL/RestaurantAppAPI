9e0c6530dc66d4d60b737e77040b0f3b1eb697e2
shuchenliu <shuchenliu2014@gmail.com>
2017-11-01 12:25:53 -0700

    fea: add ephemeral key fetch feature

--- code/resources/transaction.py
+++ code/resources/transaction.py
@@ -1,12 +1,10 @@
 from flask_restful import Resource,reqparse
 from flask_jwt import jwt_required,current_identity
-import traceback, stripe
+import traceback, stripe, config
 
 from models.user import UserModel
 
-stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
-
-BLANK_ERROR = '{} cannot be null'
+BLANK_ERROR = '{} cannot be blank'
 NOT_FOUND_ERROR: 'User {} not found'
 NOT_VALID_ERROR: 'User {} is not valid'
 DUPLICATE_ERROR: 'Customer already created'
@@ -14,85 +12,26 @@ NULL_CUSTOMER_ERROR: 'Customer not yet created'
 INTERNAL_ERROR: 'Internal server error! Failed to {}.'
 SUCCESS = 'Customer sucessfully {}'
 
-class CreateCustomer(Resource):
+stripe.api_key = config.stripe_api_key
+class GetEphemeralKey(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('uid', type=int, required=True, help=BLANK_ERROR.format('User ID'))
-    parser.add_argument('token', type=str, required=True, help=BLANK_ERROR.format('Token'))
-
-    def post(self):
-        data = self.parser.parse_args()
-        user = UserModel.find_by_id(data['uid'])
-
-        if not user:
-            return {'message' : NOT_FOUND_ERROR.format(data['uid'])}, 404
-
-        if user.stripe_id:
-            return {'message' : DUPLICATE_ERROR}, 402
-
-        if not user.email:
-            return {'message' : NOT_VALID_ERROR.format('email')}, 404
-
-        token = data['token']
-
-        try:
-            customer = stripe.Customer.create(
-                email = user.email,
-                source = token,
-            )
-        except:
-            traceback.print_exc()
-            return {'message' : INTERNAL_ERROR.format('create a new customer')}, 500
-
-        user.stripe_id = customer.id
-        try:
-            user.save_to_db()
-        except:
-            traceback.print_exc()
-            return {'message': INTERNAL_ERROR.format('save the new customer to database')}, 500
-
-        return {'message':SUCCESS.format('created')}, 200
-
-class ChargeCustomer(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('amount', type=str, required=True, help=BLANK_ERROR.format('Amount'))
+    parser.add_argument('stripe_api_version', type=str, required=True, help=BLANK_ERROR.format('API version'))
 
     @jwt_required()
     def post(self):
         data = self.parser.parse_args()
-        user = current_identity
 
-        if not user.stripe_id:
-            return {'message' : NULL_CUSTOMER_ERROR}, 404
-        try:
-            stripe.Charge.create(
-                amount=data['amount'],
-                currency="usd",
-                customer=user.stripe_id,
-            );
-        except:
-            traceback.print_exc();
-            return {'message': INTERNAL_ERROR.format('charge')}, 500
+        stripe_api_version = data['stripe_api_version']
+        customer_id = current_identity.stripeID
 
-        return {'message':SUCCESS.format('charged')}, 200
+        if not customer_id:
+            return {'message': NULL_CUSTOMER_ERROR}, 404
 
-class OneTimeCharge(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('amount', type=int, required=True, help=BLANK_ERROR.format('Amount'))
-    parser.add_argument('token', type=str, required=True, help=BLANK_ERROR.format('Token'))
+        ephemeral_key = stripe.EphemeralKey.create(customer=customer_id, api_version=stripe_api_version)
 
-    @jwt_required()
-    def post(self):
-        data = self.parser.parse_args()
-        user = current_identity
+        if not ephemeral_key:
+            return {'message': NULL_CUSTOMER_ERROR.format('create ephemeral key')}, 404
 
-        try:
-            stripe.Charge.create(
-                amount=data['amount'],
-                currency="usd",
-                source = data['token'],
-            );
-        except:
-            traceback.print_exc();
-            return {'message': INTERNAL_ERROR.format('charge')}, 500
-
-        return {'message':SUCCESS.format(user.id)}, 200
+        return {
+            'ephemeral_key': ephemeral_key
+        }, 200
