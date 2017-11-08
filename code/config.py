946aea0898c11380164d8e9d7eb030df85135115
Li Yin <li.yin.cristiano@rutgers.edu>
2017-11-08 16:08:30 -0500

    mess with config.py

--- code/config.py
+++ code/config.py
@@ -1,2 +1,4 @@
-# app configuration
-# stored in server only, not available in version control system
+stripe_api_key = "sk_test_gdX4sF9M5loorsXip54kZasF"
+stripe_publishable_key = "pk_test_cvvvvcgwdJl04LlL1u7i97Pn"
+app_secret_key = "app_sk_lfaw8io3Ife4FjeGgeh6dfa05dhka96nfuwb"
+db_splite_url = "sqlite:///restaurant.db"
