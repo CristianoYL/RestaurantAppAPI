170e5c71ba06c7e9b144538bde0d429ddb269d7c
Li Yin <li.yin.cristiano@rutgers.edu>
2017-11-08 18:28:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #empty file for server code structure
-stripe_api_key = ""
-stripe_publishable_key = ""
-app_secret_key = ""
-db_splite_url = ""
+stripe_api_key = "test"
+stripe_publishable_key = "test"
+app_secret_key = "test"
+db_splite_url = "test"
 
