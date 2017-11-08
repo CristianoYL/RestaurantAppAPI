cd7a41a75274790a7c1c9c2c710485faa8f1eaec
Li Yin <li.yin.cristiano@rutgers.edu>
2017-11-08 16:11:17 -0500

    Delete config.py

--- code/config.py
+++
@@ -1,4 +0,0 @@
-stripe_api_key = "sk_test_gdX4sF9M5loorsXip54kZasF"
-stripe_publishable_key = "pk_test_cvvvvcgwdJl04LlL1u7i97Pn"
-app_secret_key = "app_sk_lfaw8io3Ife4FjeGgeh6dfa05dhka96nfuwb"
-db_splite_url = "sqlite:///restaurant.db"
